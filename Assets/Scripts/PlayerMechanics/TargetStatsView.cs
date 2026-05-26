183943dd7721fd6be29876fe53bb48c8e5270b20
VoprakAlex <voprakalex@mail.ru>
2026-05-26 13:10:44 +0300

    update

--- Assets/Scripts/PlayerMechanics/TargetStatsView.cs
+++ Assets/Scripts/PlayerMechanics/TargetStatsView.cs
@@ -14,11 +14,27 @@ public class TargetStatsView : MonoBehaviour
     [SerializeField] public TMP_Text MaxSpeedText;
     [SerializeField] public TMP_Text CurrentSpeedText;
 
+    [Header("Damage Type Multipliers")]
+    [SerializeField] public TMP_Text SlashingMultiplierText;
+    [SerializeField] public TMP_Text PiercingMultiplierText;
+    [SerializeField] public TMP_Text BludgeoningMultiplierText;
+
+    [Header("Damage Affinity Multipliers")]
+    [SerializeField] public TMP_Text PhysicalMultiplierText;
+    [SerializeField] public TMP_Text TremorMultiplierText;
+    [SerializeField] public TMP_Text PoisonMultiplierText;
+    [SerializeField] public TMP_Text BleedMultiplierText;
+    [SerializeField] public TMP_Text ElectricMultiplierText;
+    [SerializeField] public TMP_Text FireMultiplierText;
+    [SerializeField] public TMP_Text ColdMultiplierText;
+    [SerializeField] public TMP_Text MindMultiplierText;
+
     private StatsComponent _statsComponent;
     private HealthComponent _healthComponent;
     private StaggerComponent _staggerComponent;
     private SanityComponent _sanityComponent;
     private SpeedComponent _speedComponent;
+    private ResistanceComponent _resistanceComponent;
 
     private GameObject CurrentTarget;
 
@@ -55,6 +71,7 @@ public class TargetStatsView : MonoBehaviour
         _staggerComponent = CurrentTarget.GetComponent<StaggerComponent>();
         _sanityComponent = CurrentTarget.GetComponent<SanityComponent>();
         _speedComponent = CurrentTarget.GetComponent<SpeedComponent>();
+        _resistanceComponent = CurrentTarget.GetComponent<ResistanceComponent>();
     }
 
     public void UpdateAll()
@@ -72,6 +89,9 @@ public class TargetStatsView : MonoBehaviour
         UpdateMinSpeed();
         UpdateCurrentSpeed();
         UpdateMaxSpeed();
+
+        UpdateDamageTypes();
+        UpdateDamageAffinities();
     }
 
     public void UpdateName()
@@ -119,6 +139,45 @@ public class TargetStatsView : MonoBehaviour
         MaxSpeedText.text = _statsComponent.MaxSpeed.ToString();
     }
 
+    public void UpdateDamageTypes()
+    {
+        SlashingMultiplierText.text =
+            _resistanceComponent.CurrentDamageTypeResistances[DamageType.Slashing].GetMultiplier().ToString("0.00");
+
+        PiercingMultiplierText.text =
+            _resistanceComponent.CurrentDamageTypeResistances[DamageType.Piercing].GetMultiplier().ToString("0.00");
+
+        BludgeoningMultiplierText.text =
+            _resistanceComponent.CurrentDamageTypeResistances[DamageType.Bludgening].GetMultiplier().ToString("0.00");
+    }
+
+    public void UpdateDamageAffinities()
+    {
+        PhysicalMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Physical].GetMultiplier().ToString("0.00");
+
+        TremorMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Tremor].GetMultiplier().ToString("0.00");
+
+        PoisonMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Poison].GetMultiplier().ToString("0.00");
+
+        BleedMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Bleed].GetMultiplier().ToString("0.00");
+
+        ElectricMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Electric].GetMultiplier().ToString("0.00");
+
+        FireMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Fire].GetMultiplier().ToString("0.00");
+
+        ColdMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Cold].GetMultiplier().ToString("0.00");
+
+        MindMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Mind].GetMultiplier().ToString("0.00");
+    }
+
     public void ClearTexts()
     {
         NameText.text = "";
@@ -134,5 +193,18 @@ public class TargetStatsView : MonoBehaviour
         MinSpeedText.text = "";
         CurrentSpeedText.text = "";
         MaxSpeedText.text = "";
+
+        SlashingMultiplierText.text = "";
+        PiercingMultiplierText.text = "";
+        BludgeoningMultiplierText.text = "";
+
+        PhysicalMultiplierText.text = "";
+        TremorMultiplierText.text = "";
+        PoisonMultiplierText.text = "";
+        BleedMultiplierText.text = "";
+        ElectricMultiplierText.text = "";
+        FireMultiplierText.text = "";
+        ColdMultiplierText.text = "";
+        MindMultiplierText.text = "";
     }
 }
--- Assets/Scripts/PlayerMechanics/UnitStatsView.cs
+++ Assets/Scripts/PlayerMechanics/UnitStatsView.cs
@@ -18,12 +18,27 @@ public class UnitStatsView : MonoBehaviour
     [SerializeField] public TMP_Text MaxSpeedText;
     [SerializeField] public TMP_Text CurrentSpeedText;
 
-    [Header("Components")]
-    [SerializeField] public StatsComponent _statsComponent;
-    [SerializeField] public HealthComponent _healthComponent;
-    [SerializeField] public StaggerComponent _staggerComponent;
-    [SerializeField] public SanityComponent _sanityComponent;
-    [SerializeField] public SpeedComponent _speedComponent;
+    [Header("Damage Type Multipliers")]
+    [SerializeField] public TMP_Text SlashingMultiplierText;
+    [SerializeField] public TMP_Text PiercingMultiplierText;
+    [SerializeField] public TMP_Text BludgeoningMultiplierText;
+
+    [Header("Damage Affinity Multipliers")]
+    [SerializeField] public TMP_Text PhysicalMultiplierText;
+    [SerializeField] public TMP_Text TremorMultiplierText;
+    [SerializeField] public TMP_Text PoisonMultiplierText;
+    [SerializeField] public TMP_Text BleedMultiplierText;
+    [SerializeField] public TMP_Text ElectricMultiplierText;
+    [SerializeField] public TMP_Text FireMultiplierText;
+    [SerializeField] public TMP_Text ColdMultiplierText;
+    [SerializeField] public TMP_Text MindMultiplierText;
+
+    private StatsComponent _statsComponent;
+    private HealthComponent _healthComponent;
+    private StaggerComponent _staggerComponent;
+    private SanityComponent _sanityComponent;
+    private SpeedComponent _speedComponent;
+    private ResistanceComponent _resistanceComponent;
 
     private void Awake()
     {
@@ -47,6 +62,7 @@ public class UnitStatsView : MonoBehaviour
         _staggerComponent = target.GetComponent<StaggerComponent>();
         _sanityComponent = target.GetComponent<SanityComponent>();
         _speedComponent = target.GetComponent<SpeedComponent>();
+        _resistanceComponent = target.GetComponent<ResistanceComponent>();
     }
 
     public void Show()
@@ -80,6 +96,10 @@ public class UnitStatsView : MonoBehaviour
         UpdateMinSpeed();
         UpdateCurrentSpeed();
         UpdateMaxSpeed();
+
+
+        UpdateDamageTypes();
+        UpdateDamageAffinities();
     }
 
     public void UpdateName()
@@ -127,6 +147,45 @@ public class UnitStatsView : MonoBehaviour
         MaxSpeedText.text = _statsComponent.MaxSpeed.ToString();
     }
 
+    public void UpdateDamageTypes()
+    {
+        SlashingMultiplierText.text =
+            _resistanceComponent.CurrentDamageTypeResistances[DamageType.Slashing].GetMultiplier().ToString("0.00");
+
+        PiercingMultiplierText.text =
+            _resistanceComponent.CurrentDamageTypeResistances[DamageType.Piercing].GetMultiplier().ToString("0.00");
+
+        BludgeoningMultiplierText.text =
+            _resistanceComponent.CurrentDamageTypeResistances[DamageType.Bludgening].GetMultiplier().ToString("0.00");
+    }
+
+    public void UpdateDamageAffinities()
+    {
+        PhysicalMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Physical].GetMultiplier().ToString("0.00");
+
+        TremorMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Tremor].GetMultiplier().ToString("0.00");
+
+        PoisonMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Poison].GetMultiplier().ToString("0.00");
+
+        BleedMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Bleed].GetMultiplier().ToString("0.00");
+
+        ElectricMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Electric].GetMultiplier().ToString("0.00");
+
+        FireMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Fire].GetMultiplier().ToString("0.00");
+
+        ColdMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Cold].GetMultiplier().ToString("0.00");
+
+        MindMultiplierText.text =
+            _resistanceComponent.CurrentDamageAffinityResistances[DamageAffinity.Mind].GetMultiplier().ToString("0.00");
+    }
+
     public void ClearTexts()
     {
         NameText.text = "";
@@ -142,5 +201,18 @@ public class UnitStatsView : MonoBehaviour
         MinSpeedText.text = "";
         CurrentSpeedText.text = "";
         MaxSpeedText.text = "";
+
+        SlashingMultiplierText.text = "";
+        PiercingMultiplierText.text = "";
+        BludgeoningMultiplierText.text = "";
+
+        PhysicalMultiplierText.text = "";
+        TremorMultiplierText.text = "";
+        PoisonMultiplierText.text = "";
+        BleedMultiplierText.text = "";
+        ElectricMultiplierText.text = "";
+        FireMultiplierText.text = "";
+        ColdMultiplierText.text = "";
+        MindMultiplierText.text = "";
     }
 }
